e2e88e6ce3bac1e9e10dfceaea18b798ba68150b
Andres Fernando Castillo Plata <plataandres26@gmail.com>
2024-11-03 17:54:41 -0500

    Creacion de la App Widgets funcionales en flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_widgets_funcionales
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_widgets_funcionales/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_widgets_funcionales
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
