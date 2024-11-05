3ec82c014884856f4ad3f099dd6d614cf33aaf0f
Andres Fernando Castillo Plata <plataandres26@gmail.com>
2024-11-04 19:58:59 -0500

    se crean nuevas screens y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:flutter_widgets_funcionales/presentation/screens/animated/animated_screen.dart';
+export 'package:flutter_widgets_funcionales/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:flutter_widgets_funcionales/presentation/screens/buttons/buttons_screen.dart';
 export 'package:flutter_widgets_funcionales/presentation/screens/cards/cards_screen.dart';
 export 'package:flutter_widgets_funcionales/presentation/screens/home/home_screen.dart';
+export 'package:flutter_widgets_funcionales/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:flutter_widgets_funcionales/presentation/screens/progress/progress_screen.dart';
+export 'package:flutter_widgets_funcionales/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:flutter_widgets_funcionales/presentation/ui_control/ui_control_screen.dart';
