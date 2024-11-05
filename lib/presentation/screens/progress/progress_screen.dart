54cb0e43b278e163b147c7cfb1d67709c9af100b
Andres Fernando Castillo Plata <plataandres26@gmail.com>
2024-11-05 08:17:02 -0500

    se agregan los diferentes tipos de progress indicators

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -7,6 +7,86 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const _ProgressView();
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pantalla Progress Indicators'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            const Text('Circular progress indicator'),
+            const SizedBox(height: 20),
+            CircularProgressIndicator(
+              strokeWidth: 2,
+              backgroundColor: Colors.black45,
+              color: colors.primary,
+            ),
+            const SizedBox(height: 40),
+            const Text('Circular & lineal progress indicator controlado'),
+            const SizedBox(height: 20),
+            const _ControlledProgressIndicator(),
+            const SizedBox(height: 40),
+            const Text('Refresh progress indicator'),
+            const SizedBox(height: 20),
+            RefreshProgressIndicator(
+              backgroundColor: colors.primary,
+              color: Colors.white,
+              strokeWidth: 2,
+              elevation: 2,
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return StreamBuilder<double>(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return ((value * 2) % 100) / 100; // 0.0 - 1.0
+        }).takeWhile((value) => value <= 1.0),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0.0;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                  value: progressValue,
+                  strokeWidth: 2,
+                  backgroundColor: Colors.black12,
+                  color: colors.primary,
+                ),
+                const SizedBox(width: 25),
+                Expanded(
+                  child: LinearProgressIndicator(
+                    value: progressValue,
+                    color: colors.primary,
+                  ),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
