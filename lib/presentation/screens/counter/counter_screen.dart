417a70244516650d65491f6294cbe35be5e7f20c
Andres Fernando Castillo Plata <plataandres26@gmail.com>
2024-11-16 10:50:50 -0500

    se implementa Riverpod como estado, se agrega icono usando tambien manejo de estado entre counter screen.

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_widgets_funcionales/presentation/providers/counter_provider.dart';
+import 'package:flutter_widgets_funcionales/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter - Riverpod'),
+        actions: [
+          IconButton(
+            icon: Icon(isDarkMode
+                ? Icons.dark_mode_outlined
+                : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update(
+                    (state) => !isDarkMode,
+                  );
+            },
+          )
+        ],
       ),
       body: Center(
-          child:
-              Text('Valor: 0', style: Theme.of(context).textTheme.titleLarge)),
+        child: Text('Valor: $clickCounter',
+            style: Theme.of(context).textTheme.titleLarge),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
         child: const Icon(Icons.add),
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ),
     );
   }
